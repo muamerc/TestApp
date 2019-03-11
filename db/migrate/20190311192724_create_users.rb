0d8e2a73c6fb80f90ee31ff69ec7e0572edd896f
Muamer Ćesko <cesko@ideasw.net>
2019-03-11 21:17:02 +0100

    First commit

---
+++ db/migrate/20190311192724_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users, {:id => false} do |t|
+      t.numeric :id
+      t.string :email
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.datetime :birthdate
+      t.datetime :last_login
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
