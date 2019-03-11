6d7a6caeb333f0b915b3b1b72678fc8e993bfbd1
Muamer Ćesko <cesko@ideasw.net>
2019-03-12 00:38:08 +0100

    Twys

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_11_221012) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
